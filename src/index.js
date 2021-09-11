855db58df133e7bea46751ce077418e469633e55
Konstantinos Kasidiaris <kkasidiaris@gmail.com>
2021-09-11 16:52:27 +0300

    removed typescript support, it will be a later feature

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import express from 'express';
+const app = express();
+app.get('/', (req, res) => {
+    res.send('Well done!');
+});
+app.listen(3000, () => {
+    console.log('The application is listening on port 3000!');
+});
